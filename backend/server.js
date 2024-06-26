44a6a15852358e3b8519b468cbb99fc4f880b075
Arushi Goyal <2021ceb1012@iitrpr.ac.in>
2024-06-27 03:06:56 +0530

    changes done

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://main--dsaplatform.netlify.app',
+  origin: 'https://dsaplatform.netlify.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
