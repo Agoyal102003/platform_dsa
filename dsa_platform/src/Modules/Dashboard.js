0fbfbedeec97e08d85bae93a9ad91b7101b91f11
Agoyal102003 <135025785+Agoyal102003@users.noreply.github.com>
2024-11-30 02:31:48 +0530

    Update Dashboard.js

--- dsa_platform/src/Modules/Dashboard.js
+++ dsa_platform/src/Modules/Dashboard.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Header from '../components/header';
-import Part1 from '../components/Part1';
+import Background from "../components/Background";
+// import Part1 from '../components/Part1';
 import Part2 from '../components/Part2';
 import Part3 from '../components/Part3';
 import Part4 from '../components/Part4';
 import Part5 from '../components/Part5';
 import Footer from '../components/Footer';
+import './Dashboard.css';
 
 function Dashboard() {
     return (
-        <div style={{backgroundColor: "#0a0e0f"}}>
+        <div style={{backgroundColor: "white"}}>
             <Header />
-            <Part1 />
+            <Background />
+            {/* <Part1 /> */}
             <Part2 />
             <Part3 />
             <Part4 />
