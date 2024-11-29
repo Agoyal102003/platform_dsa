bf156d4bf6a0c300dfe0a9a75151aae879fdbf13
Agoyal102003 <135025785+Agoyal102003@users.noreply.github.com>
2024-11-30 02:37:54 +0530

    Update Footer.js

--- dsa_platform/src/components/Footer.js
+++ dsa_platform/src/components/Footer.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
     return (
-        <div className="container">
+        <div className="container" style={{background: "black", maxWidth: "1700px"}}>
             <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top ft">
                 <div className="col mb-3">
                     <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
